2de38aa1ddf1a94df42929f35acd516dc83d87a6
hackerhihello <hackerhihello123@gmail.com>
2024-10-30 17:38:38 +0530

    updateed

--- app/api/events.tsx
+++ app/api/events.tsx
@@ -3,8 +3,15 @@ import axios from 'axios';
 const API_URL = 'https://backendscheduler.onrender.com/api/events';
 // const API_URL = 'http://localhost:5000/api/events';
 
+interface EventData {
+  title: string;
+  description: string;
+  date: string; // Change according to your data structure
+  // Add any other relevant fields
+}
+
 // Create a new event
-export const createEvent = async (eventData, token) => {
+export const createEvent = async (eventData: EventData, token: string) => {
   try {
     const response = await axios.post(API_URL, eventData, {
       headers: { Authorization: `Bearer ${token}` },
@@ -16,7 +23,7 @@ export const createEvent = async (eventData, token) => {
 };
 
 // Get all events
-export const getEvents = async (token,storedUserId) => {
+export const getEvents = async (token: string, storedUserId: string) => {
   try {
     const response = await axios.get(`${API_URL}/:${storedUserId}`, {
       headers: { Authorization: `Bearer ${token}` },
@@ -28,7 +35,7 @@ export const getEvents = async (token,storedUserId) => {
 };
 
 // Delete an event
-export const deleteEvent = async (eventId, token) => {
+export const deleteEvent = async (eventId: string, token: string) => {
   try {
     await axios.delete(`${API_URL}/${eventId}`, {
       headers: { Authorization: `Bearer ${token}` },
@@ -39,7 +46,7 @@ export const deleteEvent = async (eventId, token) => {
 };
 
 // Edit an event
-export const editEvent = async (eventId, eventData, token) => {
+export const editEvent = async (eventId: string, eventData: EventData, token: string) => {
   try {
     const response = await axios.put(`${API_URL}/${eventId}`, eventData, {
       headers: { Authorization: `Bearer ${token}` },
