59c7c5fa4f23b02c181b8763d90aeb68d5c440cc
hackerhihello <hackerhihello123@gmail.com>
2024-10-30 15:41:53 +0530

    update code

---
+++ declarations.d.ts
@@ -0,0 +1,8 @@
+declare module 'react-big-calendar' {
+    import * as React from 'react';
+  
+    // Define the Calendar and momentLocalizer types as needed.
+    export const Calendar: React.ComponentType<any>;
+    export const momentLocalizer: (moment: any) => void;
+  }
+  
